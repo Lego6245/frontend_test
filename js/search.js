692de76d288649a27498964c78fea4ca06fae615
William Saulnier <lego6245@gmail.com>
2014-02-28 20:19:50 -0500

    the citation thing was bugging me, so i changed it.

--- js/search.js
+++ js/search.js
@@ -1,7 +1,11 @@
 function search(value)
 {
 	var text = document.getElementById("search_text").textContent;
+	//search gloably and without sense to case
 	var searchReg = new RegExp(value, "gi");
+	var citeReg = new RegExp('(\\[.*?\\])');
+	text = text.replace(citeReg, '');
+	console.log(text);
 	var count = 0;
 	while ((match = searchReg.exec(text)) != null ) {
 		count++;
@@ -14,6 +18,7 @@ window.onload=function() {
 
 	document.getElementById("search_form").onsubmit = function(e) {
 		search(document.getElementById("search_input").value);
+		//prevents that nasty jumping effect
 		return false;
 	}
 }
